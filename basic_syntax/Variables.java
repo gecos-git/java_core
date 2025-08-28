6c03459a6051adfbb6bd5429d0a12fa22e59a944
Andrey <gecos.home@yandex.ru>
2025-08-28 06:54:53 +0000

    variables

---
+++ basic_syntax/Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int number;
+        number = 10;
+
+        int count = 5;
+
+        double price = 25.99;
+        char letter = 'J';
+        boolean isJavaFun = true;
+
+        int a = 4;
+        int b = 2;
+        int sum = a+b; 
+        double result = a/b;
+
+    
+        System.out.println(number);
+        System.out.println(count);
+        System.out.println(price);
+        System.out.println(price);
+        System.out.println(isJavaFun);
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(sum);
+        System.out.println(result);
+        System.out.println((double) result);
+    }
+}
