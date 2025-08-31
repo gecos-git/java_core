c4a9594c746195aee30ff37f43e9a6d6b1d8f538
Andrey <gecos.home@yandex.ru>
2025-08-31 16:41:21 +0000

    Создание и вывод

---
+++ basic_syntax/ArrayCreateOut.java
@@ -0,0 +1,8 @@
+public class ArrayCreateOut {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Элемент с индексом " + i + ": " + numbers[i]);
+        }
+    }
+}
