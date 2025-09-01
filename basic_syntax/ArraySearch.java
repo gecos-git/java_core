706727c23ce41f0caf32b34450e0911ea74d8b02
Andrey <gecos.home@yandex.ru>
2025-09-01 10:36:21 +0000

    Добавлен поиск элемента

--- basic_syntax/ArraySearch.java
+++ basic_syntax/ArraySearch.java
@@ -1,4 +1,19 @@
 public class ArraySearch {
     public static void main(String[] args) {
         int numbers[] = {33, 7, 43, 18, 22};
+        int target = 43;
+        boolean isFound = false;
 
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                System.out.println("Число " + target + " найдено на позиции " + i);
+                isFound = true;
+                break;
+            }
+        }
+
+        if (!isFound) {
+            System.out.println("Число " + target + " не найденов массиве");
+        }
+    }
+}
